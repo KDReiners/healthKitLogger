a19c1c9a28a5bf54e558a3a5f10fdfb16ce85204
KDReiners <kdreiners@gmx.de>
2022-03-14 19:18:06 +0100

    Initial Commit

---
+++ healthKitLogger WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  healthKitLogger WatchKit Extension
+//
+//  Created by Klaus-Dieter Reiners on 14.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ healthKitLogger WatchKit Extension/healthKitLoggerApp.swift
@@ -0,0 +1,19 @@
+//
+//  healthKitLoggerApp.swift
+//  healthKitLogger WatchKit Extension
+//
+//  Created by Klaus-Dieter Reiners on 14.03.22.
+//
+
+import SwiftUI
+
+@main
+struct healthKitLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
