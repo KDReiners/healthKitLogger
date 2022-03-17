ddf8f7abbbae84976f65c5f43c5983b71a5abeb6
KDReiners <kdreiners@gmx.de>
2022-03-17 17:19:19 +0100

    Beginning

--- healthKitLogger WatchKit Extension/ContentView.swift
+++ healthKitLogger WatchKit Extension/ContentView.swift
@@ -9,8 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        GeometryReader { reader in
+            let rect = reader.frame(in: .global)
+            VStack(spacing: 15) {
+                HStack(spacing: 25) {
+                    NavigationLink(destination: AddNutrition(), label: {
+                        NavButton(image: "plus", title: "Nutrition", rect: rect, color: .pink)
+                    })
+                        .buttonStyle(PlainButtonStyle())
+                    NavigationLink(destination: AddMedication(), label: {
+                    NavButton(image: "cross.case", title: "Medication", rect: rect, color: .blue)
+                    })
+                        .buttonStyle(PlainButtonStyle())
+                }
+                .frame(width: rect.width, alignment: .center)
+                HStack(spacing: 25) {
+                    NavButton(image: "doc.plaintext", title: "View", rect: rect, color: .pink)
+                    NavButton(image: "doc.plaintext", title: "View", rect: rect, color: .blue)
+                }
+            }
+        }
+    }
+}
+struct NavButton: View {
+    var image: String
+    var title: String
+    var rect: CGRect
+    var color: Color
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Image(systemName: image)
+                .font(.title2)
+                .frame(width: rect.width/3, height: rect.width/3, alignment: .center)
+                .background(color)
+                .clipShape(Circle())
+            Text(title)
+                .font(.system(size: 10))
+                .foregroundColor(.white)
+        }
     }
 }
 
--- healthKitLogger WatchKit Extension/healthKitLoggerApp.swift
+++ healthKitLogger WatchKit Extension/healthKitLoggerApp.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-
+import healthKitPackage
 @main
 struct healthKitLoggerApp: App {
     var body: some Scene {
